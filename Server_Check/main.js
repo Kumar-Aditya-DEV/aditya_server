694d5f9df52db283d754661a9fbb0a95622fc755
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-02-17 18:01:41 +0530

    chore: add .gitignore for node_modules, dist and env files

--- Server_Check/main.js
+++ Server_Check/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Arjun", role: "student" },
@@ -14,8 +14,7 @@ app.get("/", (req, res) => {
   res.send("Express server is running");
 });
 
-app.get("/users/test/:users_id", (req, res) => {
-    console.log("this is req:", req.params);
+app.get("/users", (req, res) => {
     res.status(200).json(users);
 });
 
@@ -31,6 +30,41 @@ app.get("/users/:id", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: req.body.id,
+    name: req.body.name,
+    role: req.body.role
+  };
+
+  users.push(newUser);
+
+  res.status(201).send("added user");
+});
+
+
+app.put("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const index = users.findIndex(u => u.id === userId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users[index] = {
+    id: userId,
+    name: req.body.name,
+    role: req.body.role
+  };
+
+  res.status(200).json({
+    message: "User replaced",
+    user: users[index]
+  });
+});
+
+
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
 });
