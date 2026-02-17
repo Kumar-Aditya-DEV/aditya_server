2b31667bf974c2689a07ee5a4e0aefe82c4e19d4
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-02-17 17:22:08 +0530

    Project initialization with basic server setup

---
+++ Student-server/server.js
@@ -0,0 +1,125 @@
+// const express = require("express");
+// const app = express();
+
+// const students = [
+//   {
+//     studentName: "ABDUL HAQUE",
+//     University: "SUxCG 714",
+//     UniversityUID: "108444"
+//   },
+//   {
+//     studentName: "ADITYA KUMAR",
+//     University: "SUxCG 702",
+//     UniversityUID: "108716"
+//   },
+//   {
+//     studentName: "AMAN KUMAR",
+//     University: "SUxCG 702",
+//     UniversityUID: "108500"
+//   },
+//   {
+//     studentName: "AMRIT RAJ",
+//     University: "SUxCG 702",
+//     UniversityUID: "108587"
+//   },
+//   {
+//     additional_info :"For specific student details write after /cg/your gr no."
+//   }
+// ];
+
+
+// app.get("/cg/students", (req, res) => {
+//   res.json(students);
+// });
+
+
+// app.get("/cg/students/:gr_number", (req, res) => {
+//   const grNumber = req.params.gr_number;
+//   const student = students.find((s) => s.UniversityUID === grNumber);
+
+//   if (student) {
+//     res.json({
+//       name: student.studentName,
+//       id: student.UniversityUID,
+//       University: student.University
+//     });
+//   } else {
+//     res.status(404).json({ message: "Student not found" });
+//   }
+// });
+
+
+
+// app.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require("express");
+const app = express();
+
+
+// this is in memory database 
+const students = [
+  {
+    studentName: "ABDUL HAQUE",
+    University: "SUxCG 714",
+    UniversityUID: "108444"
+  },
+  {
+    studentName: "ADITYA KUMAR",
+    University: "SUxCG 702",
+    UniversityUID: "108716"
+  },
+  {
+    studentName: "AMAN KUMAR",
+    University: "SUxCG 702",
+    UniversityUID: "108500"
+  },
+  {
+    studentName: "AMRIT RAJ",
+    University: "SUxCG 702",
+    UniversityUID: "108587"
+  }
+];
+
+app.get("/cg/students", (req, res) => {
+  res.json(students);
+});
+
+app.get("/cg/students/:gr_number", (req, res) => {
+  const grNumber = req.params.gr_number;
+
+  const student = students.find(
+    (s) => s.UniversityUID === grNumber
+  );
+
+  if (!student) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+
+  res.json({
+    name: student.studentName,
+    id: student.UniversityUID,
+    University: student.University
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
